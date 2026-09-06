69d14a8f108dfbf3c29c67f9e8e0fa2c3b839f1a
veleika <veleikakina07@gmail.com>
2026-09-07 08:25:46 +1000

    Fix W02 Temple Album

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,38 +1,25 @@
-```javascript
-// ------------------------------
-// Current Year
-// ------------------------------
-
 const currentYear = new Date().getFullYear();
 
 document.querySelector("#currentyear").textContent = currentYear;
 
-
-// ------------------------------
-// Last Modified Date
-// ------------------------------
-
 document.querySelector("#lastModified").textContent =
-    `Last Modification: ${document.lastModified}`;
-
-
-// ------------------------------
-// Hamburger Menu
-// ------------------------------
+`Last Modification: ${document.lastModified}`;
 
 const menuButton = document.querySelector("#menu-button");
-const navigation = document.querySelector("nav");
+const navigation = document.querySelector("#navigation");
 
 menuButton.addEventListener("click", () => {
-    navigation.classList.toggle("open");
+navigation.classList.toggle("open");
 
-    const isOpen = navigation.classList.contains("open");
-
-    menuButton.setAttribute("aria-expanded", isOpen);
-
-    menuButton.setAttribute(
-        "aria-label",
-        isOpen ? "Close navigation menu" : "Open navigation menu"
-    );
-});
 ```
+const isOpen = navigation.classList.contains("open");
+
+menuButton.setAttribute("aria-expanded", isOpen);
+
+menuButton.setAttribute(
+    "aria-label",
+    isOpen ? "Close navigation menu" : "Open navigation menu"
+);
+```
+
+});
